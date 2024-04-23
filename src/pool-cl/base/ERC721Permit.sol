25abd483ee1e5e429460e13a6b62d0da9bc04d43
Chef Snoopy <snoopy@pancakeswap.com>
2024-04-23 18:01:55 +0800

    feat: Add ERC721PermitLib

--- src/pool-cl/base/ERC721Permit.sol
+++ src/pool-cl/base/ERC721Permit.sol
@@ -3,10 +3,8 @@
 pragma solidity ^0.8.19;
 
 import {ERC721Enumerable, ERC721} from "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
-import {Address} from "@openzeppelin/contracts/utils/Address.sol";
-import {IERC1271} from "@openzeppelin/contracts/interfaces/IERC1271.sol";
-
 import {IERC721Permit} from "../interfaces/IERC721Permit.sol";
+import {ERC721PermitLib} from "../libraries/ERC721PermitLib.sol";
 
 /// @title ERC721 with permit
 /// @notice Nonfungible tokens that support an approve via signature, i.e. permit
@@ -28,22 +26,14 @@ abstract contract ERC721Permit is ERC721Enumerable, IERC721Permit {
 
     /// @inheritdoc IERC721Permit
     function DOMAIN_SEPARATOR() public view override returns (bytes32) {
-        return keccak256(
-            abi.encode(
-                /// @dev keccak256('EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)')
-                0x8b73c3c69bb8fe3d512ecc4cf759cc79239f7b179b0ffacaa9a75d522b39400f,
-                nameHash,
-                versionHash,
-                block.chainid,
-                address(this)
-            )
-        );
+        return ERC721PermitLib.DOMAIN_SEPARATOR(nameHash, versionHash);
     }
 
     /// @inheritdoc IERC721Permit
     /// @dev Value is equal to keccak256("Permit(address spender,uint256 tokenId,uint256 nonce,uint256 deadline)");
-    bytes32 public constant override PERMIT_TYPEHASH =
-        0x49ecf333e5b8c95c40fdafc95c1ad136e8914a8fb55e9dc8bb01eaa83a2df9ad;
+    function PERMIT_TYPEHASH() external pure override returns (bytes32) {
+        return ERC721PermitLib.PERMIT_TYPEHASH;
+    }
 
     /// @inheritdoc IERC721Permit
     function permit(address spender, uint256 tokenId, uint256 deadline, uint8 v, bytes32 r, bytes32 s)
@@ -51,36 +41,9 @@ abstract contract ERC721Permit is ERC721Enumerable, IERC721Permit {
         payable
         override
     {
-        if (block.timestamp > deadline) {
-            revert PermitExpired();
-        }
-
-        bytes32 digest = keccak256(
-            abi.encodePacked(
-                "\x19\x01",
-                DOMAIN_SEPARATOR(),
-                keccak256(abi.encode(PERMIT_TYPEHASH, spender, tokenId, _getAndIncrementNonce(tokenId), deadline))
-            )
+        ERC721PermitLib.permitCheck(
+            spender, tokenId, deadline, v, r, s, ownerOf(tokenId), DOMAIN_SEPARATOR(), _getAndIncrementNonce(tokenId)
         );
-        address owner = ownerOf(tokenId);
-        if (spender == owner) {
-            revert ApproveToOneself();
-        }
-
-        if (Address.isContract(owner)) {
-            /// @dev cast 4 isValidSignature(bytes32,bytes) == 0x1626ba7e
-            if (IERC1271(owner).isValidSignature(digest, abi.encodePacked(r, s, v)) == 0x1626ba7e) {
-                revert Unauthorized();
-            }
-        } else {
-            address recoveredAddress = ecrecover(digest, v, r, s);
-            if (recoveredAddress == address(0)) {
-                revert InvalidSignature();
-            }
-            if (recoveredAddress != owner) {
-                revert Unauthorized();
-            }
-        }
 
         _approve(spender, tokenId);
     }
